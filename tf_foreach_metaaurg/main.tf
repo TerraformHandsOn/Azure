d000e4b2853b7ca4b2583592dd447c74706df446
Omkar Birader <omkar.birader@microsoft.com>
2021-10-08 18:45:33 +0530

    Added foreach poc

---
+++ tf_foreach_metaaurg/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">=2.0"
+     }
+  }
+}
+
+# Provider block
+provider "azurerm" {
+  features {
+    
+  }
+}
---
+++ tf_foreach_metaaurg/resource-group.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "myrg" {
+    for_each = {
+        dc1apps = "eastus"
+        dc2apps = "eastus2"
+        dc3apps = "westus"
+    }
+    name = "${each.key}myrg"
+    location = each.value
+}
